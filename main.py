794ff003d52dd76139c094804a8f9ce4cb54e6cd
Thomas Cheng <contact.chengthomas@gmail.com>
2021-04-16 19:26:38 -0400

    added discord bot functionality

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from numpy import uint8
 from numpy.random import random_integers, choice
 # this import is for image cropping
@@ -7,11 +8,16 @@ import skimage.filters
 import skimage.io
 # from skimage.viewer import ImageViewer
 import requests
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
 
 POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon/'
 MAX_POKEDEX_INDEX = 898
 SIGMA = 25
 
+pokemon_name = ""
 
 def get_random_url():
 	p_id = random_integers(1,MAX_POKEDEX_INDEX)
@@ -22,19 +28,16 @@ def get_random_url():
 def apply_gaussian_blur(image_url, name):
 	# read and display original image
 	image = skimage.io.imread(fname=image_url)
-	skimage.io.imsave(name+"-og-image.png", image)
+	skimage.io.imsave("pokemon.png", image)
 	# apply Gaussian blur, creating a new image
 	blurred = skimage.filters.gaussian(
 	    image, sigma=(SIGMA, SIGMA), truncate=3.5, multichannel=True)
-	# # display blurred image
-	# viewer = ImageViewer(blurred)
-	# viewer.show()
-	skimage.io.imsave(name+"-blurred.png", blurred)
+	skimage.io.imsave("whos-that-pokemon.png", blurred)
 
 
 def apply_zoom_and_crop(image_url, name):
 	image = Image.open(requests.get(image_url, stream=True).raw)
-	image.save(name + "-og-image.png")
+	image.save("pokemon.png")
 	width, height = image.size
 	center_x, center_y = width//2, height//2
 	min_dimension = min(width, height)
@@ -45,10 +48,12 @@ def apply_zoom_and_crop(image_url, name):
 	# top, left, right, bottom
 	cropped_image = image.crop((center_x-size, center_y-size, center_x+size, center_y+size))\
 			.resize((width,height), Image.ANTIALIAS)
-	cropped_image.save(name + "-cropped.png")
+	cropped_image.save("whos-that-pokemon.png")
 
 
-def main():
+def get_new_pokemon():
+	global pokemon_name
+
 	# make the get request to the pokeAPI
 	random_url = get_random_url()
 	r = requests.get(url = random_url)
@@ -56,6 +61,8 @@ def main():
 
 	image_url = data['sprites']['other']['official-artwork']['front_default']
 	name = data['name']
+	pokemon_name = name.lower()
+	print(pokemon_name)
 
 	action = choice([True, False])
 	if action:
@@ -63,5 +70,41 @@ def main():
 	else:
 		apply_gaussian_blur(image_url, name)
 
+	
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix='?')
+client = discord.Client()
 
-main()
+
+@bot.event
+async def on_ready():
+    # guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{bot.user.name} is connected'
+    )
+
+
+@bot.command(name='play')
+async def play(ctx):
+	get_new_pokemon()
+	await ctx.send(file=discord.File('whos-that-pokemon.png'))
+
+
+@bot.command(name='reveal')
+async def reveal(ctx):
+	print(pokemon_name)
+	await ctx.send('It\'s {}!'.format(pokemon_name),file=discord.File('pokemon.png'))
+
+
+@bot.command(name='guess')
+async def guess(ctx, arg):
+	arg = arg.strip().lower()
+	print(arg + ", " + pokemon_name)
+	if arg == pokemon_name:
+		await ctx.send('You guessed right!', file=discord.File('pokemon.png'))
+	else:
+		await ctx.send('Nope, try again!')
+
+
+bot.run(TOKEN)
