be84d31f1a7cfdb5113fd550812af7ef8407620d
Thomas Cheng <contact.chengthomas@gmail.com>
2021-04-16 13:38:25 -0400

    moved code to main function

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
-from numpy.random import random_integers
+from numpy import uint8
+from numpy.random import random_integers, choice
 # this import is for image cropping
 from PIL import Image
 # this import is for Gaussian blurring
-import skimage
+import skimage.filters
+import skimage.io
 # from skimage.viewer import ImageViewer
 import requests
-# from io import BytesIO
 
 POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon/'
 MAX_POKEDEX_INDEX = 898
 SIGMA = 25
+
 
 def get_random_url():
 	p_id = random_integers(1,MAX_POKEDEX_INDEX)
 	random_url = POKEMON_URL + str(p_id)
 	return random_url
 
-# make the get request to the pokeAPI
-random_url = get_random_url()
-r = requests.get(url = random_url)
-data = r.json()
-
-image_url = data['sprites']['other']['official-artwork']['front_default']
-name = data['name']
 
 def apply_gaussian_blur(image_url, name):
 	# read and display original image
 	image = skimage.io.imread(fname=image_url)
+	skimage.io.imsave(name+"-og-image.png", image)
 	# apply Gaussian blur, creating a new image
 	blurred = skimage.filters.gaussian(
 	    image, sigma=(SIGMA, SIGMA), truncate=3.5, multichannel=True)
 	# # display blurred image
 	# viewer = ImageViewer(blurred)
 	# viewer.show()
-	skimage.io.imsave("test-image.png", blurred)
+	skimage.io.imsave(name+"-blurred.png", blurred)
 
 
 def apply_zoom_and_crop(image_url, name):
@@ -51,4 +47,21 @@ def apply_zoom_and_crop(image_url, name):
 			.resize((width,height), Image.ANTIALIAS)
 	cropped_image.save(name + "-cropped.png")
 
-apply_zoom_and_crop(image_url, name)
+
+def main():
+	# make the get request to the pokeAPI
+	random_url = get_random_url()
+	r = requests.get(url = random_url)
+	data = r.json()
+
+	image_url = data['sprites']['other']['official-artwork']['front_default']
+	name = data['name']
+
+	action = choice([True, False])
+	if action:
+		apply_zoom_and_crop(image_url, name)
+	else:
+		apply_gaussian_blur(image_url, name)
+
+
+main()
